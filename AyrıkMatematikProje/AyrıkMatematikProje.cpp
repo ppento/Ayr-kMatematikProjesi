eed3c60380a2fe5bd57b539112a11d50dcfd07c5
ppento <utkugecgel1905@gmail.com>
2025-05-17 15:40:27 +0300

    Proje dosyası ekle.

---
+++ AyrıkMatematikProje/AyrıkMatematikProje.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include<locale.h>
+
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "Turkish");
+    int b, c;
+    cout << "b değerini giriniz: ";
+    cin >> b;
+    cout << "c değerini giriniz: ";
+    cin >> c;
+
+    bool found = false;
+    cout << "Tamsayı çözümlerden bazıları (x, y):" << endl;
+
+    for (int y = -10; y <= 10; y++) {
+        int numerator = c - b * y;
+        if (numerator % 2 == 0) {
+            int x = numerator / 2;
+            cout << "(" << x << ", " << y << ")" << endl;
+            found = true;
+        }
+    }
+
+    if (!found) {
+        cout << "Bu değerler için tamsayı çözüm bulunamadı." << endl;
+    }
+
+    return 0;
+}
